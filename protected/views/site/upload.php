4df79dec1f865aaa7cbf508031feb29b0d3c7144
vincentzheng <miraculous368@163.com>
2013-06-08 09:17:41 +0800

    not update

--- protected/views/site/upload.php
+++ protected/views/site/upload.php
@@ -2,7 +2,7 @@
 <?php echo CHtml::form('','POST',array('enctype'=>'multipart/form-data'));?>
 <?php echo CHtml::activeFileField($model,'excelfile');?>
 <?php echo CHtml::errorSummary($model);?>
-<?php echo CHtml::submitButton('Submit');?>
+<?php echo CHtml::submitButton('Submit',array('value'=>"提交"));?>
 <?php echo CHtml::endForm();?>
 <script type="text/javascript">
 //这里检测是否有文件提交
